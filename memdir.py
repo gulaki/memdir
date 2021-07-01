554834d2476ffccc9c8b02f14d6d6f3e321ad18c
pythonphysicsteacher <pythonphysicsteacher@gmail.com>
2021-07-01 15:48:59 +0530

    Added documentation and type annotations. Added `testdir` to load and copy the directory.

--- memdir.py
+++ memdir.py
@@ -1,18 +1,49 @@
+"""
+Module `memdir`
+
+This module provides the functionality to represent a tree data structure that mimics the
+behaviour of directories and files.
+
+The class `MemDir` represents a node in this tree data structure and behaves like a directory
+that can contain files (data containing objects) and subdirectories (`MemDir` objects).
+
+The objective is to be able to create data in a certain hierarchy, save the entire data tree
+to disk while preserving the directory structure, and load the data back from disk, while
+preserving the directory structure.
+
+A specific use case is to use `MemDir` trees with `fluidmidi` and `theorymuse` MIDI sequencers
+to procedurally generate clip libraries, that can be exported to disk and imported in MIDI
+capable digital audio software.
+"""
 import os
 
 path = os.path.dirname(__file__)
 EXTN = '.dat'
 
 def loader(file):
+    """A function used by `load_path` to read individual files and populate into a `MemDir` object.
+
+    Arg:
+        file (str): File name with relative or absolute path.
+
+    Returns: The data read from a file that is populated into the `MemDir`.
+    """
     with open(file, 'r') as f:
         data = f.read()
     return data
 
 def writer(file, data):
+    """A function used by the disk dump mechanism to save the data on disk.
+
+    Args:
+        file (str): Filename with path.
+        data:       Data to write to disk.
+    """
     with open(file, 'w') as f:
         f.write(str(data))
 
 class MemDir(dict):
+    """A node in the tree data structure."""
     def __init__(self, name):
         super().__init__()
         self.parent = None
@@ -27,22 +58,28 @@ class MemDir(dict):
             parent = parent.parent
         return path
 
-    def create_child(self, newdir):
+    def create_child(self, newdir: 'MemDir' or str) -> 'MemDir':
+        """Create a new child node under current node."""
         if type(newdir) == str:
             newdir = MemDir(name=newdir)
         newdir.parent = self
         super().__setitem__(newdir.name, newdir)
         return newdir
 
-    def add_obj(self, obj):
+    def add_obj(self, obj) -> 'MemDir':
+        """Insert a data object in this node."""
         self.files.append(obj)
         return self
 
-    def add_objs(self, objs):
+    def add_objs(self, objs) -> 'MemDir':
+        """Insert multiple data objects in this node."""
         self.files.extend(objs)
         return self
 
     def traverse(self):
+        """Similar to `os.walk()`. This function is an iterator that walks through
+        the tree and yields every node and subnode.
+        """
         yield self
         for dir in self.values():
             yield from dir.traverse()
@@ -66,7 +103,8 @@ class MemDir(dict):
             else:
                 return super().__getitem__(first)[rest]
 
-    def make_subdirs(self, path):
+    def make_subdirs(self, path: str) -> 'MemDir':
+        """Behaves like `os.makedirs`. """
         first, rest = self.__split_first_rest__(path)
         if first not in self:
             self.create_child(first)
@@ -81,7 +119,7 @@ class MemDir(dict):
         # self.make_subdirs(key)
         # self[key].add_obj(value)
 
-    def rename(self, newname):
+    def rename(self, newname: str) -> None:
         if self.parent is None:
             self.name = newname
         else:
@@ -90,7 +128,7 @@ class MemDir(dict):
             pop.name = newname
             parent.create_child(pop)
 
-    def copy(self, newname=None):
+    def copy(self, newname: str = None) -> 'MemDir':
         files = []
         for file in self.files:
             try:
@@ -103,16 +141,16 @@ class MemDir(dict):
             newdir.create_child(dir.copy())
         return newdir
 
-    def numfiles(self):
+    def numfiles(self) -> int:
         return len(self.files)
 
-    def numdirs(self):
+    def numdirs(self) -> int:
         return len(self)
 
     def __repr__(self):
         return f"MemDir('{self.get_path()}', D={self.numdirs()}, F={self.numfiles()})"
 
-def load_path(path):
+def load_path(path: str) -> MemDir:
     tree = MemDir('.')
     for root, dirs, files in os.walk(path):
         root = root.replace('\\', '/')
@@ -125,7 +163,7 @@ def load_path(path):
     del tree
     return root
 
-def dump_tree(root, memdir):
+def dump_tree(root: str, memdir: MemDir):
     oldname = memdir.name
     memdir.name = root
     for dir in memdir.traverse():
@@ -135,7 +173,7 @@ def dump_tree(root, memdir):
             writer(os.path.join(dirpath, file[0]), file[1])
     memdir.name = oldname
 
-def dump_data(root, memdir):
+def dump_data(root: str, memdir: MemDir):
     oldname = memdir.name
     memdir.name = root
     for dir in memdir.traverse():
@@ -146,8 +184,8 @@ def dump_data(root, memdir):
     memdir.name = oldname
 
 if __name__ == '__main1__':
-    memdir = load_path('D:/MusicLab/memdir/test1 - Copy')
-    dump_tree(os.path.join('D:/dumptree1/copy'), memdir)
+    memdir = load_path('./testdir')
+    dump_tree(os.path.join('./copytestdir'), memdir)
 
 if __name__ == '__main__':
     memdir = MemDir('.')
